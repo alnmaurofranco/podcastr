7cd5c8a8d8de7e048d69c7a41be716e9e85ec443
alnmaurofranco <jefeson23@live.com>
2021-07-18 01:58:36 -0300

    :white_check_mark: update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://api-jobsmanager.herokuapp.com/api'
+  baseURL: 'http://localhost:3333' // 'https://api-jobsmanager.herokuapp.com/api'
 })
